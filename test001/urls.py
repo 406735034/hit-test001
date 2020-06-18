170ee991eabcdf0ed2069b70cfb2275eb7f0e2c2
406735034 <tvbchary@gms.tku.edu.tw>
2020-06-19 00:42:20 +0800

    After Awards

--- test001/urls.py
+++ test001/urls.py
@@ -1,24 +1,32 @@
 from django.urls import path
-from . import views 
+from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    #Index
+    # Index
     path('', views.home, name='home'),
-    #Login
+    # Login
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.register, name='register'),
-    #Reset Password
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name= "reset_password.html"), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name= "password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name="password_reset_complete"),
-    
-    #User_Home
+    # Reset Password
+    path('reset_password/', auth_views.PasswordResetView.as_view(
+        template_name="reset_password.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_done.html'), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='password_reset_confirm.html'), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset_complete.html'), name="password_reset_complete"),
+
+    # Home
     path('dashboard/', views.dashboard, name='dashboard'),
+
+    # User
     path('user/', views.userPage, name='user-page'),
-    
-    #Warnings
+    path('user-rec/', views.userRecPage, name='user-rec'),
+    path('user-awards/', views.userAwards, name='user-awards'),
+    path('user-ranks/', views.userRanks, name='user-ranks'),
+    # Warnings
     path('restrict/', views.restrict, name='restrict'),
 ]
